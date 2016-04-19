be16990556b1c8b138d6b8d5929d9a429cfd3327
William Eatman <billeatman@hotmail.com>
2016-04-18 23:47:28 -0500

    added ng-view to userman

--- includes/js/app/app.js
+++ includes/js/app/app.js
@@ -1,15 +1,38 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('marloo-cms', []);
+	var baseURL = "includes/js/app/";
+	var app = angular.module('marloo-cms', ['ngRoute']);
 
-	app.controller('UsermanController', [ '$http', function($http) {
+	app.controller('UsersController', [ '$scope', '$http', function($scope, $http) {
 		var userman = this;
 		userman.users = [];
+
 
 		$http({ method: 'GET', url: '/index.cfm/marlooauth:marlooUser/list'}).success(function(data){
 			userman.users = data;
 			console.log(userman.users);
 		});
 	} ]);
+
+	app.controller('UsermanController', ['$scope', function($scope) {
+		$scope.currentURL = '/index.cfm/marlooauth:userman/listUsers';
+	}]);
+
+	app.config(['$routeProvider', 
+		function($routeProvider){
+			$routeProvider.
+			when('/Users', {
+				templateUrl: baseURL + 'templates/users.html',
+			}).
+			when('/Groups', {
+				templateUrl: baseURL + 'templates/groups.html',
+			}).
+			when('/Roles', {
+				templateUrl: baseURL + 'templates/roles.html',
+			}).
+			otherwise({
+				redirectTo: '/Users'
+			});
+	}]);
 })();
