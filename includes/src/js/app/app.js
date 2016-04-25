15ca55fd3c9ad261a5764626c75e0bf0638a455b
William Eatman <billeatman@hotmail.com>
2016-04-24 22:38:17 -0500

    Implemented promise then to show User panel after load

--- includes/src/js/app/app.js
+++ includes/src/js/app/app.js
@@ -16,38 +16,71 @@
 		});
 	} ]);*/
 
+	app.factory('userServ', function($http, $rootScope, $q, $resource) {
+		var service = {};
+		var baseUrl = $rootScope.baseUrl;
+
+		service.listUsers = function() {
+			return $resource('/index.cfm/marlooauth/marlooUser/list').query().$promise;
+		};
+
+		service.getUser = function(username) {
+			return $resource('/index.cfm/marlooauth/marlooUser/list').query().$promise;
+		};
+
+		return service;
+	});
+
+	app.controller('UserCtrl', UserCtrl);
+	function UserCtrl($scope, $rootScope, element){
+	};
+
+
+	app.directive('user', function($rootScope) {
+		return {
+			controllerAs: 'user',
+			templateUrl: $rootScope.baseUrl + 'templates/userman/user.html'
+		};
+	});
+
 	app.controller('UsersCtrl',UsersCtrl);
-	function UsersCtrl($scope, DTOptionsBuilder, DTColumnBuilder, $filter, $resource) {
+	function UsersCtrl($scope, $rootScope, DTOptionsBuilder, DTColumnBuilder, $filter, $resource, userServ) {
 		var userman = this;
 		userman.message = ''; 
+		$scope.baseUrl = $rootScope.baseUrl;
 		userman.someClickHandler = someClickHandler;
 
+		$scope.showUser = false;
+
 		userman.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-       	return $resource('/index.cfm/marlooauth/marlooUser/list').query().$promise;
-    }).withOption('info',false).withOption('lengthChange',false).withDisplayLength(50).withOption('rowCallback', rowCallback);
-    userman.dtColumns = [
-        DTColumnBuilder.newColumn('firstName').withTitle('User').renderWith(function(data, type, full) {
-            return full.firstName + ' ' + full.lastName;
-        }),
-        DTColumnBuilder.newColumn('createdDate').withTitle('Added On').renderWith(function(data, type) {
-        		return $filter('date')(data, 'MMM dd, yyyy');
-        }),
-        DTColumnBuilder.newColumn('active').withTitle('Active?')
-    ];
+       		return userServ.listUsers();
+    	}).withOption('info',false).withOption('lengthChange',false).withDisplayLength(50).withOption('rowCallback', rowCallback);
 
-    function someClickHandler(info){
-    	userman.message = info.firstName;
-    };
+    	userman.dtColumns = [
+	        DTColumnBuilder.newColumn('firstName').withTitle('User').renderWith(function(data, type, full) {
+	            return full.firstName + ' ' + full.lastName;
+	        }),
+	        DTColumnBuilder.newColumn('createdDate').withTitle('Added On').renderWith(function(data, type) {
+	        		return $filter('date')(data, 'MMM dd, yyyy');
+	        }),
+	        DTColumnBuilder.newColumn('active').withTitle('Active?')
+	    ];
 
-    function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull){
-    	$('td', nRow).unbind('click');
-    	$('td', nRow).bind('click', function(){
-    		$scope.$apply(function() {
-    			userman.someClickHandler(aData);
-    		});
-    	});
-    };
+	    function someClickHandler(info){
+	    	userServ.getUser('billeatman@hotmail.com').then(function(){
+	  			$scope.showUser = true;
+  			});
+	    	userman.message = info.firstName;
+	    };
 
+	    function rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull){
+	    	$('td', nRow).unbind('click');
+	    	$('td', nRow).bind('click', function(){
+	    		$scope.$apply(function() {
+	    			userman.someClickHandler(aData);
+	    		});
+	    	});
+    	};
 	};
 
 	app.controller('UserManTabCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
@@ -65,5 +98,9 @@
 		$scope.isActiveTab = function(tabIndex){
 			return $scope.activeTab == tabIndex;
 		}
+	}]);
+
+	app.controller('UserCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+		$scope.username = '';
 	}]);
 })();
